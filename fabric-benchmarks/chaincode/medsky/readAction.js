53e60dd095d8aec4b813b2eb4842d13ee8480bbc
pexas <josebernardo@tecnico.ulisboa.pt>
2024-09-10 13:19:33 +0100

    extedent coookie

--- fabric-benchmarks/chaincode/medsky/readAction.js
+++ fabric-benchmarks/chaincode/medsky/readAction.js
@@ -43,7 +43,7 @@ class ReadActionWorkload extends WorkloadModuleBase {
       contractId: 'medsky',
       contractVersion: '1.0',
       contractFunction: 'ReadAction',
-      contractArguments: ['Client' + this.workerIndex + '_ACTION' + actionId],
+      contractArguments: ['Client' + this.workerIndex + '_CREATE_ACTION' + actionId],
       timeout: 30,
       readOnly: true
     };
