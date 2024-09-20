d86f6de1e1b2ff2d3d2a3be8fc36e359c212f6e4
pexas <josebernardo@tecnico.ulisboa.pt>
2024-09-20 19:44:48 +0100

    aaaa

---
+++ k6-grafana-load-testing/fhir_script_test.js
@@ -0,0 +1,51 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+import exec from 'k6/x/exec';
+import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+
+export const options = {
+  vus: 1,
+  duration: '10m',
+};
+
+const url = 'http://10.100.0.12:5555';
+const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImIzOWI0MTI2LWRlZGEtNDFiYi05ZmFjLTgxODhmZjNjMWZmYiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOiJtZWRwbHVtLWNsaSIsImxvZ2luX2lkIjoiYmJmZWQxYmMtNzg0Yi00N2Y4LTgzOGMtZjVjYmQ0MmQ0MTc4Iiwic3ViIjoiNzMxYmQ1NzktOGY2My00MGI4LTgwMWYtNDk4MWExNGZiNDVjIiwidXNlcm5hbWUiOiI3MzFiZDU3OS04ZjYzLTQwYjgtODAxZi00OTgxYTE0ZmI0NWMiLCJzY29wZSI6Im9wZW5pZCIsInByb2ZpbGUiOiJQcmFjdGl0aW9uZXIvM2RjODZhZDktZTE5YS00Y2QxLWIyMzItODdkODJiNjM0ZDM4IiwiaWF0IjoxNzI2NjU5NjgyLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjU1NTUvIiwiYXVkIjoibWVkcGx1bS1jbGkiLCJleHAiOjE3MjY2NjMyODJ9.mGAJyA6mOQ4MZmHPWDExeFXhvX0CuwwVVDe9zJuowrcS2essueHrmyWDtzdxme-is-3CV50JweE5u2qm-n_LFeGL_HkhO4J7uJnbWTLGTLu4OGeLS5sCxn1lJd1ii8Z87s801Ru9U2tF-JkgFNKc0Q0wavsmcJ5OUhgpLt8O2q0j2JIgv9enkNEX8lcTTtHNOLzMFsGWYNsyp1RfRbqrpBBaIsPx0WAuRfAeVXotiXL1AN8CS6P-pl7f3VXN5nwN0BXB6EYa82UrW80NGUYNl58jfgtn-KuUuSisEyC56L08_Ao1sJrVmYq6aHTTA6SeF0DXrdsgjj2g5Lxva6WSzQ';
+
+const SAMPLES_N = 12
+const samplesDir = 'fhir-samples'
+
+const params = {
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + token
+  }
+}
+
+const records = []
+for (let i = 1; i < SAMPLES_N; i++) {
+    records.push(JSON.parse(open(samplesDir + '/' + (i + 1) + '.json')))
+}
+
+export default function() {
+    const fhirIdx = Math.floor(Math.random() * SAMPLES_N + 1);
+    const recordId = uuidv4();
+    const actionId = uuidv4();
+
+    const command = 'sh'
+    const args = ['./invoke.sh', recordId, actionId, samplesDir + '/' + fhirIdx + '.json'];
+
+    console.log(exec.command(command, args));
+
+    const resourceType = fhirRecord.resourceType;
+
+    let res = http.post(url + `/fhir/R4/${resourceType}?recordId=${recordId}&actionId=${actionId}`, fhirRecord, params);
+    console.log(res.status);
+}
+
+/*
+export function handleSummary(data) {
+    return {
+        'metrics.json': JSON.stringify(data.metrics, null, 2),  // Save only the metrics
+    };
+}
+*/
---
+++ k6-grafana-load-testing/invoke.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+pushd ../fabric-network
+
+export FABRIC_CFG_PATH=${PWD}/configtx-large
+export PATH=${PWD}/bin:$PATH
+. scripts/envVar.sh
+. scripts/util.sh
+
+ORG=$((RANDOM % 9 + 1))
+
+RECORD_ID=$1
+ACTION_ID=$2
+HASH=$(sha256sum $3 | awk '{print($1)}')
+
+setGlobals $ORG
+
+peer chaincode invoke -o localhost:7011 --ordererTLSHostnameOverride orderer.example.com \
+     --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" \
+     -C mychannel -n medsky \
+     --peerAddresses localhost:8101 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
+     --peerAddresses localhost:8201 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
+     --peerAddresses localhost:8301 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt" \
+     --peerAddresses localhost:8401 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org4.example.com/peers/peer0.org4.example.com/tls/ca.crt" \
+     --peerAddresses localhost:8501 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org5.example.com/peers/peer0.org5.example.com/tls/ca.crt" \
+     --peerAddresses localhost:8601 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org6.example.com/peers/peer0.org6.example.com/tls/ca.crt" \
+     -c "{\"function\":\"CreateRecord\",\"Args\":[\"$RECORD_ID\", \"$HASH\", \"$ACTION_ID\"]}"
+
+popd
