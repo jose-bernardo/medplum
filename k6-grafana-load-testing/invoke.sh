3f461d8f30acc039715a78a85db55d5a9eb6732d
pexas <josebernardo@tecnico.ulisboa.pt>
2024-09-22 14:26:03 +0100

    aaaa

--- k6-grafana-load-testing/invoke.sh
+++ k6-grafana-load-testing/invoke.sh
@@ -16,20 +16,20 @@ HASH=$3
 setGlobals $ORG
 
 if [ -z "$3" ]; then
-    peer chaincode invoke -o localhost:7011 --ordererTLSHostnameOverride orderer.example.com \
+    peer chaincode invoke -o orderer.example.com:7011 --ordererTLSHostnameOverride orderer.example.com \
      --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" \
      -C mychannel -n medsky \
-     --peerAddresses localhost:8101 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
-     --peerAddresses localhost:8201 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
-     --peerAddresses localhost:8301 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt" \
+     --peerAddresses peer0.org1.example.com:8101 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
+     --peerAddresses peer0.org2.example.com:8201 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
+     --peerAddresses peer0.org3.example.com:8301 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt" \
      -c "{\"function\":\"ReadRecordTx\",\"Args\":[\"$RECORD_ID\", \"$ACTION_ID\"]}"
 else
     peer chaincode invoke -o localhost:7011 --ordererTLSHostnameOverride orderer.example.com \
      --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" \
      -C mychannel -n medsky \
-     --peerAddresses localhost:8101 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
-     --peerAddresses localhost:8201 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
-     --peerAddresses localhost:8301 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt" \
+     --peerAddresses peer0.org1.example.com:8101 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
+     --peerAddresses peer0.org2.example.com:8201 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
+     --peerAddresses peer0.org3.example.com:8301 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt" \
      -c "{\"function\":\"CreateRecord\",\"Args\":[\"$RECORD_ID\", \"$HASH\", \"$ACTION_ID\"]}"
 fi
 
