1a650c8c4ab1769f76b7f4c678b8da8539d1cbbc
pexas <josebernardo@tecnico.ulisboa.pt>
2024-10-01 00:12:30 +0100

    simple

--- k6-grafana-load-testing/test.sh
+++ k6-grafana-load-testing/test.sh
@@ -4,11 +4,11 @@ function pre_test_warmup() {
   export DURATION=$1
   export VUS=10
   echo "Sending binary files"
-  ./k6 run ./binaryWriteTest.js &
+  ./k6 run ./binaryWriteTest.js > /dev/null &
 
   echo "Sending FHIR data"
   export VUS=40
-  ./k6 run ./fhirWriteTest.js
+  ./k6 run ./fhirWriteTest.js > /dev/null
 
   echo "Finished warmup"
 }
@@ -20,14 +20,11 @@ function infoln() {
 # Expires 30 Out 2024
 export TOKEN="eyJhbGciOiJSUzI1NiIsImtpZCI6IjY3M2ZmYTYxLWI4MDctNGQ2Ni1hZWY4LTYxMmEyYTcyYTE4YSIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOiJtZWRwbHVtLWNsaSIsImxvZ2luX2lkIjoiNGExYmQ0ZDAtZjExYi00YmQxLWIwMmQtNWFkZmEzYzliNDkxIiwic3ViIjoiY2Y0OTkwMDQtZWIwMS00NjJmLWFjZTEtNjc1MDMzMDQwN2E4IiwidXNlcm5hbWUiOiJjZjQ5OTAwNC1lYjAxLTQ2MmYtYWNlMS02NzUwMzMwNDA3YTgiLCJzY29wZSI6Im9wZW5pZCIsInByb2ZpbGUiOiJQcmFjdGl0aW9uZXIvNjkwYjRmNjAtZmI3OS00OGFlLWI1YTktZTEzYWVmNTM5ZWExIiwiaWF0IjoxNzI3NzM1Mjc5LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjU1NTUvIiwiYXVkIjoibWVkcGx1bS1jbGkiLCJleHAiOjE3MzAzMjcyNzl9.kiPyIVpPJEQb5TNXJE1Jxa27yHL4SB9NK_kLdphRdYy4krO0FTy8TPryMRKkU7M9E9gQJ4PNQBBg4MqB0MRZ0FsSxI9Fw5JxiFftQixSaVA6SzIIXYEWyvCkVWoPom0vII0Peejj_aOW5J9e1dfROGhHwaRvAAIKHtEscfldhNpcd8huFzfnM3OvxJiMSoth7E94gvi0g6vPq1s0yWZWyBrNEwtyYX97YgHz4JyUrBw2yZNZ78JJ1ZmQ7oytNr3KiUsy8zKz-j4GqYn2_Tg1tGRzNHLPBVJAT9Kq1v__gQDQjS9y2R6u9VFAacZXq3wA3Qi-rZcP_rnjC1wfVEm0_A"
 
-echo "##########################################"
-echo "####### Starting System Evaluation #######"
-echo "##########################################"
-
 mkdir -p reports
 
-pre_test_warmup 10m
 
+function execFhirWriteTest() {
+pre_test_warmup 5m
 export VUS=50
 export DURATION=65m
 
@@ -36,9 +33,11 @@ infoln "Start Time: $(date)" | tee -a log
 K6_WEB_DASHBOARD=true K6_WEB_DASHBOARD_EXPORT=reports/fhirWriteTest.html ./k6 run ./fhirWriteTest.js
 infoln "End Time: $(date)" >> log.txt
 infoln " -------------------- "
+}
 
+
+function execFhirReadTest() {
 pre_test_warmup 5m
-
 export VUS=50
 export DURATION=65m
 
@@ -47,9 +46,11 @@ infoln "Start Time: $(date)" | tee -a log
 K6_WEB_DASHBOARD=true K6_WEB_DASHBOARD_EXPORT=reports/fhirReadTest.html ./k6 run ./fhirReadTest.js
 infoln "End Time: $(date)" >> log.txt
 infoln " -------------------- "
+}
 
+
+function execBinaryReadTest() {
 pre_test_warmup 5m
-
 export VUS=10
 export DURATION=65m
 
@@ -58,9 +59,11 @@ infoln "Start Time: $(date)"
 K6_WEB_DASHBOARD=true K6_WEB_DASHBOARD_EXPORT=reports/binaryWriteTest.html ./k6 run ./binaryReadTest.js
 infoln "End Time: $(date)"
 infoln " -------------------- "
+}
 
+
+function execBinaryWriteTest() {
 pre_test_warmup 5m
-
 export VUS=10
 export DURATION=15m
 
@@ -69,6 +72,26 @@ infoln "Start Time: $(date)"
 K6_WEB_DASHBOARD=true K6_WEB_DASHBOARD_EXPORT=reports/binaryWriteTest.html ./k6 run ./binaryWriteTest.js
 infoln "End Time: $(date)"
 infoln " -------------------- "
+}
+
+echo "##########################################"
+echo "####### Starting System Evaluation #######"
+echo "##########################################"
+
+if [ $1 = "fr" ]; then
+  execFhirReadTest
+elif [ $1 = "fw" ]; then
+  execFhirWriteTest
+elif [ $1 = "br" ]; then
+  execBinaryReadTest
+elif [ $1 = "bw" ]; then
+  execBinaryWriteTest
+else
+  execFhirReadTest
+  execFhirWriteTest
+  execBinaryReadTest
+  execBinaryWriteTest
+fi
 
 echo "#########################################"
 echo "####### Ending System Evaluation ########"
